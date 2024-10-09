765bb219d2a544bf01f396b36c36861ec2ef19bc
mitchellkolb <mkolb321@gmail.com>
2024-10-08 19:38:00 -0700

    I will have to look into openGL some other time to fix the problem of button elements not appearing when the window is running

--- theLab/main.py
+++ theLab/main.py
@@ -1,23 +1,14 @@
 
 
-# # main.py
-# from kivy.app import App
-# from kivy.uix.widget import Widget
-
-# class MainWidget(Widget):
-#     pass
-
-# class MyApp(App):
-#     def build(self):
-#         return MainWidget()
-
-# if __name__ == "__main__":
-#     MyApp().run()
-
 
 from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    pass
 
 class TheLabApp(App):
-    pass
+    def build(self):
+        return MainWidget() 
 
 TheLabApp().run()
