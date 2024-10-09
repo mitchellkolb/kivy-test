108fe35bd21d192994bb1daf9fb7695541d8b2b8
mitchellkolb <mkolb321@gmail.com>
2024-10-08 19:23:59 -0700

    Added the venv and boilerplate

---
+++ theLab/main.py
@@ -0,0 +1,15 @@
+
+
+# main.py
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    pass
+
+class MyApp(App):
+    def build(self):
+        return MainWidget()
+
+if __name__ == "__main__":
+    MyApp().run()
