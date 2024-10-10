97df4de412599b07c5b1cb9a273cccdc5c44d160
mitchellkolb <mkolb321@gmail.com>
2024-10-09 20:01:31 -0700

    Literally don't know what I'm doing at times. Might have to reset the down thing becuase openGL is giving me errors

--- theLab/main.py
+++ theLab/main.py
@@ -1,14 +1,28 @@
 
 
 
-from kivy.app import App
-from kivy.uix.widget import Widget
+# from kivy.app import App
+# from kivy.uix.widget import Widget
 
-class MainWidget(Widget):
-    pass
+# class MainWidget(Widget):
+#     pass
+
+# class TheLabApp(App):
+#     def build(self):
+#         return MainWidget() 
+
+# TheLabApp().run()
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class TheLab(BoxLayout):
+    def say_hello(self):
+        print("hello")
 
 class TheLabApp(App):
     def build(self):
-        return MainWidget() 
+        return TheLab()
 
-TheLabApp().run()
+if __name__ == "__main__":
+    TheLabApp().run()
