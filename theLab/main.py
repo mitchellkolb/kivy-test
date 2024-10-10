9ba093d40eb1246cc3b5ad21f82423d8153443dc
mitchellkolb <mkolb321@gmail.com>
2024-10-09 20:19:21 -0700

    Pong code is displaying. This is progress

--- theLab/main.py
+++ theLab/main.py
@@ -2,31 +2,46 @@
 
 
 # from kivy.app import App
-# from kivy.uix.widget import Widget
+# from kivy.uix.screenmanager import ScreenManager, Screen
+# from kivy.uix.button import Button
 
-# class MainWidget(Widget):
-#     pass
-
-# class TheLabApp(App):
+# class MyScreenApp(App):
 #     def build(self):
-#         return MainWidget() 
-
-# TheLabApp().run()
+#         sm = ScreenManager()
+        
+#         # Create a Screen w/1 button and add it
+#         main = Screen(name='main')
+#         main.add_widget(Button(text='main'))
+#         sm.add_widget(main)
+        
+#         return sm
+        
+# if __name__ == '__main__':
+#     MyScreenApp().run()
 
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
 
-class MyScreenApp(App):
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
+class PongGame(Widget):
+    pass
+
+
+class PongApp(App):
     def build(self):
-        sm = ScreenManager()
-        
-        # Create a Screen w/1 button and add it
-        main = Screen(name='main')
-        main.add_widget(Button(text='main'))
-        sm.add_widget(main)
-        
-        return sm
-        
+        return PongGame()
+
+
 if __name__ == '__main__':
-    MyScreenApp().run()
+    PongApp().run()
