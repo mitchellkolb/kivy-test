c7807c4a9068692292ee09cca7617710812e7ac1
mitchellkolb <mkolb321@gmail.com>
2024-10-15 20:56:55 -0700

    Finally got a white box to appear at the buttom when following a new tutorial. TBH idk what the kivy docs and my other tutorial have incorrect but I'm going to move forward with this if it allows me to work

--- main.py
+++ main.py
@@ -2,9 +2,19 @@
 
 
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 
-class FirstApp(App):
-    pass
+class MyBoxLayout(BoxLayout):
+    def OnSubmit(self):
+        TextInput = self.ids.TextInput
+        InputText = TextInput.text
+        OutputLabel = self.ids.OutputLabel
+        OutputLabel.text = InputText
+        TextInput.text = ''
+
+class MyApp(App):
+    def build(self):
+        return MyBoxLayout()
 
 if __name__ == "__main__":
-    FirstApp().run()
+    MyApp().run()
